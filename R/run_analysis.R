f6393eb706e7f3ad0e6df77b6e44af1a838e4647
Clara-Lea <50104903+claralea@users.noreply.github.com>
2022-12-07 10:22:08 -0500

    fix bugs

--- R/run_analysis.R
+++ R/run_analysis.R
@@ -4,7 +4,7 @@
 #' @param output.path string with the path to the folder to save the results
 #' @param siteid string
 #' @return dataframe containing the results for all outcomes of interest
-#' @import SuperLearner data.table dplyr tidyr partykit rpart
+#' @import SuperLearner data.table dplyr tidyr partykit rpart IsingFit bnlearn
 #' @importFrom stats  as.formula binomial cov gaussian glm na.omit predict
 #' @importFrom utils write.csv
 #' @export
@@ -23,22 +23,22 @@ run_analysis = function(input.path, output.path, siteid="BI", threshold=0){
   }
   "Reading the data..."
   data.input = create.table(input.path)
-  
+
   # count the empirical distribution of X
   print("Estimating distribution of X...")
   X = as.matrix(rbind(data.input$X.train, data.input$X.test))
-  
-  #### Ising Model using "IsingFit" package #### 
-  Ising.model <- IsingFit(X, family='binomial', plot=FALSE, progressbar=TRUE) 
-  
+
+  #### Ising Model using "IsingFit" package ####
+  Ising.model <- IsingFit(X, family='binomial', plot=FALSE, progressbar=TRUE)
+
   #### Bayesian Network using "bnlearn" package ####
   X_df <- data.frame(X)
   X_df[,1:ncol(X_df)] <- lapply(X_df[,1:ncol(X_df)], factor)
-  structure.bic <- hc(data.frame(X_df), score = "ebic") 
+  structure.bic <- hc(data.frame(X_df), score = "ebic")
   bn.mod.bic <- bn.fit(structure.bic, data = X_df)
-  structure.bde <- hc(data.frame(X_df), score = "bde") 
+  structure.bde <- hc(data.frame(X_df), score = "bde")
   bn.mod.bde <- bn.fit(structure.bde, data = X_df)
-  
+
   #### count nx > threshold ####
   p <- dim(X)[2]
   X_full <- as.matrix(expand.grid(rep(list(0:1), p)))
@@ -51,20 +51,20 @@ run_analysis = function(input.path, output.path, siteid="BI", threshold=0){
                  BayesNet.bde=bn.mod.bde,
                  xn=xn,
                  mean.X = colMeans(X))
-  
+
   X = as.matrix(data.input$X.train)
   A = as.matrix(data.input$A.train)
-  
+
   for(out in colnames(data.input$Y.train)){
     Y = as.matrix(data.frame(data.input$Y.train)[, out])
     print(paste0("Running analysis for outcome: ", out))
-    
+
     res_y = site_estimation(X, A, Y, or.model="LR")
-    
+
     res_all[[out]] = deiden(res_y$DR_fit, res_y$test_rank_x)
   }
   res_all[["test_rank_x"]] = res_y$test_rank_x
-  
+
   print(paste0("Saving the results in ", output.path))
   save(res_all, file = paste0(output.path, '/CATE_results_', siteid,".rda"))
 }
