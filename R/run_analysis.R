864d712ef10585cabedf4a333fbcf551f3e04fb8
Weijing Tang <weijtang@Weijings-Mac-Mini.local>
2022-12-14 11:37:19 -0500

    Xuan updated create.table

--- R/run_analysis.R
+++ R/run_analysis.R
@@ -4,7 +4,7 @@
 #' @param output.path string with the path to the folder to save the results
 #' @param siteid string
 #' @return dataframe containing the results for all outcomes of interest
-#' @import SuperLearner data.table dplyr tidyr partykit rpart IsingFit bnlearn
+#' @import SuperLearner data.table dplyr tidyr partykit rpart
 #' @importFrom stats  as.formula binomial cov gaussian glm na.omit predict
 #' @importFrom utils write.csv
 #' @export
@@ -15,7 +15,7 @@
 # siteid="VA"
 # load("sysdata.rda")
 
-run_analysis = function(input.path, output.path, siteid="BI", threshold=0){
+run_analysis = function(input.path, output.path, siteid="VA"){
   if (siteid=="VA"){
     threshold <- 11
   } else{
@@ -23,22 +23,23 @@ run_analysis = function(input.path, output.path, siteid="BI", threshold=0){
   }
   "Reading the data..."
   data.input = create.table(input.path)
-
+  
   # count the empirical distribution of X
   print("Estimating distribution of X...")
   X = as.matrix(rbind(data.input$X.train, data.input$X.test))
-
-  #### Ising Model using "IsingFit" package ####
-  Ising.model <- IsingFit(X, family='binomial', plot=FALSE, progressbar=TRUE)
-
-  #### Bayesian Network using "bnlearn" package ####
   X_df <- data.frame(X)
   X_df[,1:ncol(X_df)] <- lapply(X_df[,1:ncol(X_df)], factor)
-  structure.bic <- hc(data.frame(X_df), score = "ebic")
-  bn.mod.bic <- bn.fit(structure.bic, data = X_df)
-  structure.bde <- hc(data.frame(X_df), score = "bde")
-  bn.mod.bde <- bn.fit(structure.bde, data = X_df)
-
+  rank_x <- sapply(droplevels(X_df), nlevels) > 1 
+  
+  #### Ising Model using "IsingFit" package #### 
+  Ising.model <- IsingFit(X[,rank_x], family='binomial', plot=FALSE, progressbar=TRUE) 
+  
+  #### Bayesian Network using "bnlearn" package ####
+  structure.bic <- hc(data.frame(X_df[,rank_x]), score = "bic") 
+  bn.mod.bic <- bn.fit(structure.bic, data = X_df[,rank_x])
+  structure.bde <- hc(data.frame(X_df[,rank_x]), score = "bde") 
+  bn.mod.bde <- bn.fit(structure.bde, data = X_df[,rank_x])
+  
   #### count nx > threshold ####
   p <- dim(X)[2]
   X_full <- as.matrix(expand.grid(rep(list(0:1), p)))
@@ -50,21 +51,23 @@ run_analysis = function(input.path, output.path, siteid="BI", threshold=0){
                  BayesNet.bic=bn.mod.bic,
                  BayesNet.bde=bn.mod.bde,
                  xn=xn,
-                 mean.X = colMeans(X))
-
+                 mean.X = colMeans(X),
+                 rank.x = rank_x,
+                 site.size = dim(X)[1])
+  
   X = as.matrix(data.input$X.train)
   A = as.matrix(data.input$A.train)
-
+  
   for(out in colnames(data.input$Y.train)){
     Y = as.matrix(data.frame(data.input$Y.train)[, out])
     print(paste0("Running analysis for outcome: ", out))
-
-    res_y = site_estimation(X, A, Y, or.model="LR")
-
+    
+    res_y = site_estimation(X, A, Y)
+    
     res_all[[out]] = deiden(res_y$DR_fit, res_y$test_rank_x)
   }
-  res_all[["test_rank_x"]] = res_y$test_rank_x
-
+  res_all[["train_rank_x"]] = res_y$test_rank_x
+  
   print(paste0("Saving the results in ", output.path))
   save(res_all, file = paste0(output.path, '/CATE_results_', siteid,".rda"))
 }
